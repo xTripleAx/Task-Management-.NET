ce6ab4367f8bf583113bb42fe609e67cbd1c1471
Ali <aliatieh77@gmail.com>
2023-12-06 08:05:15 +0200

    Model Adjustments

---
+++ TaskManagement/Models/UserProject.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.Models
+{
+    public class UserProject
+    {
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        [Key]
+        public string CompositeKey { get; set; }
+    }
+}
